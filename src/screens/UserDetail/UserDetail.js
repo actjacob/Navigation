3ce87fc8a62f8a786661b247ca2bbb7640681617
actjacob <yakupaktar99@gmail.com>
2024-10-09 16:18:01 +0300

    fetching process-2

--- src/screens/UserDetail/UserDetail.js
+++ src/screens/UserDetail/UserDetail.js
@@ -1,21 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button, findNodeHandle } from "react-native";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 import Loading from "../../components/Loading";
 
 const UserDetail = ({ route, navigation }) => {
   const { id } = route.params;
+
+  const [userId, setUserId] = useState(id);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          onPress={() => setUserId((id) => (id + 1 === 11 ? 1 : id + 1))}
+          title="Next"
+        />
+      ),
+    });
+  }, [navigation]);
+
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
       .then((res) => res.json())
       .then((data) => setUser(data))
       .finally(() => setLoading(false));
-  }, []);
+  }, [userId]);
 
   return (
     <View>
+      <Text>User ID:{userId} </Text>
       {loading ? (
         <Loading text="Loading User..." />
       ) : (
